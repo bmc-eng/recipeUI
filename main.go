9c5dc1011b19896f60f962c904024b9211969ba5
bmc-eng <bclarkeuk@googlemail.com>
2023-11-16 19:40:17 -0500

    updated for handling a template

--- main.go
+++ main.go
@@ -1,14 +1,57 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Recipe struct {
+	Name        string       `json:"name"`
+	Ingredients []Ingredient `json:"ingredients"`
+	Steps       []string     `json:"steps"`
+	Picture     string       `json:"imageURL"`
+}
+
+type Ingredient struct {
+	Quantity string `json:"quantity"`
+	Name     string `json:"name"`
+	Type     string `json:"type"`
+}
+
+var recipes []Recipe
 
 func IndexHandler(c *gin.Context) {
-	c.File("index.html")
+	recipes := make([]Recipe, 0)
+	recipes = append(recipes, Recipe{
+		Name:    "Burger",
+		Picture: "/assets/images/burger.jpg",
+	})
+	recipes = append(recipes, Recipe{
+		Name:    "Pizza",
+		Picture: "/assets/images/pizza.jpg",
+	})
+	recipes = append(recipes, Recipe{
+		Name:    "Tacos",
+		Picture: "/assets/images/tacos.jpg",
+	})
+	c.HTML(http.StatusOK, "index.tmpl", gin.H{
+		"recipes": recipes,
+	})
+}
+
+func init() {
+	recipes = make([]Recipe, 0)
+	file, _ := os.ReadFile("recipes.json")
+	_ = json.Unmarshal([]byte(file), &recipes)
 }
 
 func main() {
 	router := gin.Default()
 	router.Static("/assets", "./assets")
+	router.LoadHTMLGlob(("templates/*"))
 	router.GET("/", IndexHandler)
 	router.Run()
 }
